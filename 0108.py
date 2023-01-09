876ba3344ad6b4523d80ad307a6c7b078b44e02c
yunhee <100117015+yunhee1@users.noreply.github.com>
2023-01-09 23:43:05 +0900

    Add files via upload

--- 0108.py
+++ 0108.py
@@ -2,4 +2,18 @@ import turtle as t
 
 t.forward(100)
 t.left(120)
-t.forward
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+t.circle(50) #거북이로 반지름 50인 원 그리기
