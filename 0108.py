a4c583a785b37c0c853bf23ce0f4e32d685ec3ff
yunhee1 <alkrg007@naver.com>
2023-01-09 23:29:07 +0900

    python 거북이 그리기

---
+++ 0108.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+t.circle(50) #
